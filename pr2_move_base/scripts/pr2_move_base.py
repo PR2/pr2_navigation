23135832d201ac40c0590b59b6f1f57d97d2b9c0
Eitan Marder-Eppstein <eitan@willowgarage.com>
2011-03-24 10:36:09 -0700

    Transplanting arm tucking, checking AC power, and lowering the spine from trunk

--- pr2_move_base/scripts/pr2_move_base.py
+++ pr2_move_base/scripts/pr2_move_base.py
@@ -41,10 +41,14 @@ import roslib; roslib.load_manifest(PKG)
 
 import rospy
 import actionlib
+from std_msgs.msg import Bool
 from pr2_msgs.msg import LaserTrajCmd
+from pr2_msgs.msg import PowerState
 from pr2_msgs.srv import SetLaserTrajCmd
 from actionlib_msgs.msg import GoalStatus
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseResult, MoveBaseFeedback
+from pr2_controllers_msgs.msg import SingleJointPositionAction, SingleJointPositionGoal
+from pr2_common_action_msgs.msg import TuckArmsAction, TuckArmsGoal
 import dynamic_reconfigure.client
 from pr2_controllers_msgs.msg import PointHeadAction, PointHeadGoal
 
@@ -103,7 +107,7 @@ def restore_laser():
     try: 
         rospy.wait_for_service('tilt_hokuyo_node/set_parameters', 1.0)
     except rospy.exceptions.ROSException, e:
-        rospy.logerr("Couldn't set parameters %s" % e)
+        rospy.logerr("Couldn't set the profile on the laser. Exception %s" % e)
         return
     #end TODO
 
@@ -117,6 +121,36 @@ def on_shutdown():
 
 def execute_cb(goal):
     rospy.loginfo("Received a goal")
+
+    # check for network cable
+    if network_connected:
+        server.set_aborted(None, "Dangerous to navigate with network cable connected.")
+        return
+
+    # check for power cable
+    if ac_power_connected:
+        server.set_aborted(None, "Dangerous to navigate with AC power cable connected.")
+        return
+
+    # check for power cable
+    # TODO
+
+    # start the arms tucking
+    tuck_arms_client = actionlib.SimpleActionClient('tuck_arms', TuckArmsAction)
+    tuck_arms_client.wait_for_server()
+    tuck_goal = TuckArmsGoal()
+    tuck_goal.tuck_left=True
+    tuck_goal.tuck_right=True
+    tuck_arms_client.send_goal(tuck_goal)
+
+    # start the torso lowering
+    torso_client = actionlib.SimpleActionClient('torso_controller/position_joint_action', SingleJointPositionAction)
+    torso_client.wait_for_server()
+    torso_down_goal = SingleJointPositionGoal()
+    torso_down_goal.position = 0
+    torso_client.send_goal(torso_down_goal)
+
+    # configure the tilting laser
     if not set_tilt_profile([1.05,  -.7, 1.05], [0.0, 1.8, 2.0125 + .3]):
         server.set_aborted(MoveBaseResult(), "Couldn't set the profile on the laser")
         return
@@ -124,11 +158,39 @@ def execute_cb(goal):
     configure_laser()
     configure_head()
 
+    # wait for tuck_arms to finish. (Don't wait for torso to lower, that is a Nice To Have)
+    while not tuck_arms_client.wait_for_result(rospy.Duration(0.1)):
+        if server.is_preempt_requested():
+            if not server.is_new_goal_available():
+                tuck_arms_client.cancel_goal()
+
+    tuck_state = tuck_arms_client.get_state()
+    if tuck_state != GoalStatus.SUCCEEDED:
+        if tuck_state == GoalStatus.PREEMPTED:
+            server.set_preempted(MoveBaseResult(), "Tuck-arms preempted")
+        elif tuck_state == GoalStatus.ABORTED:
+            server.set_aborted(MoveBaseResult(), "Tuck-arms aborted")
+        return
+
+    # Now everything should be ready so send the navigation goal.
     move_base_client.send_goal(goal, None, None, feedback_cb)
 
     while not move_base_client.wait_for_result(rospy.Duration(0.1)):
+        #in the unlikely event of network cable being plugged in while moving, stop moving.
+        if network_connected:
+            move_base_client.cancel_goal()
+            server.set_aborted(None, "Dangerous to navigate with network cable connected.")
+            return
+
+        #in the unlikely event of ac power cable being plugged in while moving, stop moving.
+        if ac_power_connected:
+            move_base_client.cancel_goal()
+            server.set_aborted(None, "Dangerous to navigate with ac power cable connected.")
+            return
+
         if server.is_preempt_requested():
             if not server.is_new_goal_available():
+                rospy.loginfo("Preempt requested without new goal, cancelling move_base goal.")
                 move_base_client.cancel_goal()
 
             server.set_preempted(MoveBaseResult(), "Got preempted by a new goal")
@@ -146,10 +208,27 @@ def execute_cb(goal):
     else:
         server.set_aborted(result, "Unknown result from move_base")
 
+def handle_network_connected( connectedness_msg ):
+    global network_connected
+    network_connected = connectedness_msg.data
+
+def handle_power_state( power_state_msg ):
+    global ac_power_connected
+    ac_power_connected = (power_state_msg.AC_present > 0)
 
 if __name__ == '__main__':
     name = 'pr2_move_base'
     rospy.init_node(name)
+
+    #listen for messages about the network cable being plugged in.
+    global network_connected
+    network_connected = False #default presumes we are fine if we never hear anything.
+    rospy.Subscriber("network/connected", Bool, handle_network_connected)
+
+    #listen for messages about the power cable being plugged in.
+    global ac_power_connected
+    ac_power_connected = False #default presumes we are fine if we never hear anything.
+    rospy.Subscriber("power_state", PowerState, handle_power_state)
 
     #create the action client to move_base
     move_base_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
@@ -167,6 +246,7 @@ if __name__ == '__main__':
     old_config = {}
 
     #we'll set the tilt profile and configure the laser by default when we start up
+    #NOTE: IF YOU CHANGE THIS, ALSO MAKE SURE TO CHANGE THE tilt_lasers_filters.yaml FILE IN pr2_navigation_perception
     set_tilt_profile([1.05,  -.7, 1.05], [0.0, 1.8, 2.0125 + .3])
     configure_laser()
 
